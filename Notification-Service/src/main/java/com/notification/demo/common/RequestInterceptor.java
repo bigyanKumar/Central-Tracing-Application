6a8449c650b90d5de8ed29a51f4651a0716f3374
Bigyan Kumar Sandilya <bigyankumarnwd@gmail.com>
2023-08-19 16:24:34 +0530

    updating the log file appender

---
+++ Notification-Service/src/main/java/com/notification/demo/common/RequestInterceptor.java
@@ -0,0 +1,41 @@
+package com.notification.demo.common;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.MDC;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Component
+@Order(1)
+public class RequestInterceptor implements Filter{
+	
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		try {
+			HttpServletRequest httpRequest = (HttpServletRequest) request;
+			HttpServletResponse httpResponse = (HttpServletResponse) response;
+			String tenant = httpRequest.getHeader("tenant");
+			if(tenant!=null) {
+				httpResponse.setHeader("tenant", tenant);
+			}
+			MDC.putCloseable("tenant", tenant);
+		    chain.doFilter(request, response);
+		    MDC.clear();
+		}finally {
+			MDC.remove("tenant");
+		}
+	}
+
+}
